6cd8b252ba10c65608ddc7b1cf7efe2007fc66c3
George Leone <georgeleone047@gmail.com>
2016-04-23 17:30:41 -0400

    third commit

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
   def create
-    @article = Photo.find(params[:photo_id])
+    @photo = Photo.find(params[:photo_id])
     @comment = @photo.comments.create(comment_params)
-    redirect_to article_path(@article)
+    redirect_to article_path(@photo)
   end
 end
--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,11 +1,11 @@
 class PhotosController < ApplicationController
   def new
+    # Httparty will go here to pull the images
   end
 
   def index
     @photos = Photo.all
   end
-
 
 
 end
