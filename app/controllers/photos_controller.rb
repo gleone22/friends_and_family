603557cf7c07e8f5bd87a788d948d97b027e4c52
George Leone <georgeleone047@gmail.com>
2016-04-23 18:46:12 -0400

    fourth commit

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,6 +1,9 @@
+require 'httparty'
+
 class PhotosController < ApplicationController
   def new
-    # Httparty will go here to pull the images
+    response = HTTParty.get("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=0da305fa43a4c6834cfefe751fb703ea&lat=25.807&lon=-80.199165&radius=19&per_page=30&format=json&nojsoncallback=1")
+    JSON.parse response.body
   end
 
   def index
