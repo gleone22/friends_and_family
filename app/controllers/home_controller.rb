6aaba975e2692d8f4beb4457c646debac96eaab9
George Leone <georgeleone047@gmail.com>
2016-04-23 19:32:26 -0400

    fixed link

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -15,7 +15,7 @@ long = "-71.1273687"
 
 base_url = "https://api.flickr.com/services/rest/"
 
-params = "?method=flickr.photos.search&api_key=2a30e293c0809c659d40e281d1676862&lat=#{lat}&lon=#{long}&format=json&nojsoncallback=1"
+params = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=0da305fa43a4c6834cfefe751fb703ea&lat=#{lat}&lon=#{long}&format=json&nojsoncallback=1"
 
 full_url = base_url + params
 
